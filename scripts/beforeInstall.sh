00b00c49ece6fe8763175dfbbf8130d48345041c
Satyam Vishwakarma <87614909+satyam-vishwa@users.noreply.github.com>
2024-09-07 22:03:23 +0530

    Update beforeInstall.sh

--- scripts/beforeInstall.sh
+++ scripts/beforeInstall.sh
@@ -1,9 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
-# Stop any existing application running (if applicable)
-echo "Stopping existing application..."
-sudo systemctl stop my-web-app.service || true
-
-# Clean up previous deployment artifacts
-echo "Cleaning up old deployment files..."
-rm -rf /home/ec2-user/dist
+# Check if the /home/ubuntu/dist directory exists
+if [[ -d "/home/ubuntu/dist" ]]; then
+    # If it exists, stop and delete the api service
+    sudo pkill -9 node
+    # Remove the dist directory
+    cd /home/ubuntu && sudo rm -rf dist
+else
+    # If it doesn't exist, echo a message indicating it's a fresh EC2 instance
+    echo "Fresh EC2 instance"
+fi
