70b0e3a04fdd8708a7c20428cc971510ae8ba4b9
Satyam Vishwakarma <87614909+satyam-vishwa@users.noreply.github.com>
2024-09-07 22:03:59 +0530

    Update beforeInstall.sh

--- scripts/beforeInstall.sh
+++ scripts/beforeInstall.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Check if the /home/ubuntu/dist directory exists
-if [[ -d "/home/ubuntu/dist" ]]; then
+if [[ -d "/home/ec2-user/dist" ]]; then
     # If it exists, stop and delete the api service
     sudo pkill -9 node
     # Remove the dist directory
-    cd /home/ubuntu && sudo rm -rf dist
+    cd /home/ec2-user && sudo rm -rf dist
 else
     # If it doesn't exist, echo a message indicating it's a fresh EC2 instance
     echo "Fresh EC2 instance"
