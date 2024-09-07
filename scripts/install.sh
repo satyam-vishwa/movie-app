3fe384fef1d9a64f9b78d20341529e5a449f29fb
Satyam Vishwakarma <87614909+satyam-vishwa@users.noreply.github.com>
2024-09-07 20:20:08 +0530

    Create install.sh

---
+++ scripts/install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Navigate to the deployment directory
+cd /home/ec2-user/dist
+
+# Install Node.js and npm if they are not already installed
+if ! command -v node &> /dev/null
+then
+    echo "Node.js not found, installing..."
+    curl -sL https://rpm.nodesource.com/setup_18.x | sudo bash -
+    sudo yum install -y nodejs
+fi
+
+# Install the global serve package
+echo "Installing 'serve' globally..."
+npm install -g serve
+
+
